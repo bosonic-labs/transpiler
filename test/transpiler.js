15db6e1e503ddbc0d017e1ad913f3003233d8f1b
Raphaël Rougeron <goldoraf@gmail.com>
2014-02-22 10:33:10 +0100

    Added support for detection of script dependencies

--- test/transpiler.js
+++ test/transpiler.js
@@ -1,12 +1,22 @@
 var fs = require('fs'),
     transpiler = require('../');
 
-exports.testTranspilation = function(test) {
+exports.testSimpleTranspilation = function(test) {
     test.expect(1);
     var transpiled = transpiler(fs.readFileSync(__dirname + '/fixtures/spec_sample.html', 'utf8')),
         expected = {
             js: fs.readFileSync(__dirname + '/expected/spec_sample.js', 'utf8')
         };
     test.equal(transpiled.js, expected.js, "the JS should be transpiled");
+    test.done();
+}
+
+exports.testScriptDependency = function(test) {
+    test.expect(1);
+    var transpiled = transpiler(fs.readFileSync(__dirname + '/fixtures/sample_with_deps.html', 'utf8')),
+        expected = {
+            scripts: ["../node_modules/moment/moment.js", "../node_modules/pikaday/pikaday.js"]
+        };
+    test.deepEqual(transpiled.scripts, expected.scripts, "the script dependencies should be found");
     test.done();
 }
